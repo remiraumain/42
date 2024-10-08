6728e361f854a1a50e9735cb6622bd2dfcab872f
Remi Raumain <remiraumain@gmail.com>
2024-10-08 10:19:52 +0200

    ft_isdigit done -no test

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -1,15 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/08 09:11:10 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/08 10:19:39 by rraumain         ###   ########.fr       */
+/*   Created: 2024/10/08 10:17:55 by rraumain          #+#    #+#             */
+/*   Updated: 2024/10/08 10:19:15 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(const char *s);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
