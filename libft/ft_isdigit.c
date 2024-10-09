83703e79edfbdb84b5918273a66f13134467da99
Remi Raumain <remiraumain@gmail.com>
2024-10-09 08:07:21 +0200

    Comments on ft_isdigit and ft_isprint

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,10 +6,17 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:17:55 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/08 10:19:15 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/09 08:02:56 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks for a numeric character.
+ * 
+ * The ft_isdigit() function checks if c is a numeric character (0 through 9).
+ * 
+ * @param c Character (ASCII encoded) to check.
+ */
 int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -6,10 +6,17 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:34:21 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/08 10:39:14 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/09 08:06:28 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks for a printable character.
+ * 
+ * The ft_isprint() function checks if c is a printable character (including space).
+ * 
+ * @param c Character (ASCII encoded) to check.
+ */
 int	ft_isprint(int c)
 {
 	if (c >= ' ' && c <= '~')
