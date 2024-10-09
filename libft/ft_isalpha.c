dcbf197f6493af58fb6614a21283ebef86f1c173
Remi Raumain <remiraumain@gmail.com>
2024-10-09 07:57:15 +0200

    Comments on ft_isalpha

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,10 +6,17 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:15:29 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/08 10:15:31 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/09 07:55:54 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks for an alphabetic character.
+ * 
+ * The ft_isalpha() function checks if c is an alphanumeric character; it is equivalent to (isupper(c) || islower(c)).
+ * 
+ * @param c Character (ASCII encoded) to check.
+ */
 int	ft_isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
