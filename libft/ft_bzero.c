87c1dd8f393df46c768d4d671ebf89f4e63f8728
Remi Raumain <remiraumain@gmail.com>
2024-10-09 07:33:10 +0200

    Polished bzero function with comments and corrected: pointer casting to use correct inexing (i) with size_t

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,19 +6,30 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:49:53 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/08 10:53:19 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/09 07:16:10 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	bzero(void *s, int n)
+#include "libft.h"
+
+/**
+ * @brief Brief
+ * 
+ * Desc
+ * 
+ * @param s What is s
+ * @param n What is n 
+ */
+void	bzero(void *s, size_t n)
 {
-	int	i;
+	size_t	i;
+	char	*ptr;
 
 	i = 0;
+	ptr = (char)s;
 	while (i < n)
 	{
-		s[i] = '\0';
+		ptr[i] = '\0';
 		i++;
 	}
-	return (s);
 }
