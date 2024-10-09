cd09b11756f7e7ee167e62cb864e89278aa8634a
Remi Raumain <remiraumain@gmail.com>
2024-10-09 07:41:06 +0200

    Wrong name for the ft_bzero function

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:49:53 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/09 07:33:37 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/09 07:39:53 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 /**
  * @brief Erases n bytes of the memory  at the location pointed by s.
  * 
- * The  bzero()  function  erases  the data in the n bytes of the memory starting
+ * The  ft_bzero()  function  erases  the data in the n bytes of the memory starting
  *  at the location pointed to by s, by writing zeros (bytes containing '\0') to
  *  that area.
  * 
  * @param s Pointer of the memory's location. 
  * @param n Number of bytes to erase.
  */
-void	bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	size_t	i;
 	char	*ptr;
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:11:10 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/08 11:22:08 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/09 07:40:33 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,4 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_strlen(const char *s);
 void	*memset(void *s, int c, int n);
-void	bzero(void *s, int n);
+void	ft_bzero(void *s, size_t n);
