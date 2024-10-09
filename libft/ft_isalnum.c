ab563fceb56e0d3ad7c574e9e30418e7681889d0
Remi Raumain <remiraumain@gmail.com>
2024-10-09 07:48:59 +0200

    Comments on ft_isalnum

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,10 +6,17 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:22:14 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/08 10:26:28 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/09 07:47:54 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks for an alphanumeric character.
+ * 
+ * The ft_isalnum() function checks if c is an alphanumeric character; it is equivalent to (isalpha(c) || isdigit(c)).
+ * 
+ * @param c Character (ASCII encoded) to check.
+ */
 int	ft_isalnum(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
