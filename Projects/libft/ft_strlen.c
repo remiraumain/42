d646832a0ad9455d8746abc844e5b39bb179d940
Rémi Raumain <remiraumain@gmail.com>
2024-10-10 11:43:54 +0200

    New Structure

--- Projects/libft/ft_strlen.c
+++ Projects/libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 08:15:44 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/09 08:21:11 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/09 12:57:45 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 /**
  * @brief Calculate the length of a string.
- * 
+ *
  * The  ft_strlen() function calculates the length of the string pointed to by s,
  *  excluding the terminating null byte ('\0').
- * 
+ *
  * @param s Pointer of the string's first character (or the starting point).
  * @return The lenght of the string (number of bytes) pointed by s.
  */
-size_t	ft_strlen(const char *s)
+size_t ft_strlen(const char *s)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
 	while (s[i])
