149aa5a5eb44a8a7c0088aaf62858432a1e7d40f
Rémi Raumain <remiraumain@gmail.com>
2024-10-16 15:04:08 +0200

    Test: ft_strlcat | Fixed

--- Projects/libft/ft_strlcat.c
+++ Projects/libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 10:43:02 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/14 11:09:36 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/16 15:03:08 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,24 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	j;
+	size_t	k;
+	size_t	cpy_len;
 
 	i = 0;
 	j = 0;
-	while (dst[i])
+	k = 0;
+	while (i < size && dst[i] != '\0')
 		i++;
-	while (src[j] && j < size)
-	{
-		if (size > 0)
-		{
-			if (i + j < size - 1)
-				dst[i + j] = src[j];
-			else
-				dst[i + j] = '\0';
-		}
+	while (src[j] != '\0')
 		j++;
+	if (size <= i)
+		return (size + j);
+	cpy_len = size - i - 1;
+	while (k < cpy_len && src[k] != '\0')
+	{
+		dst[i + k] = src[k];
+		k++;
 	}
-	if (size > 0 && (i + j) <= size)
-		dst[i + j] = '\0';
+	dst[i + k] = '\0';
 	return (i + j);
 }
--- Projects/libft/libft.h
+++ Projects/libft/libft.h
@@ -22,6 +22,7 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_atoi(const char *nptr);
