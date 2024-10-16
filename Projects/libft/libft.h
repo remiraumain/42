3a83585528cb8eeeaf6507f45f0e4373b2d24573
Rémi Raumain <remiraumain@gmail.com>
2024-10-16 16:29:43 +0200

    why did I do strstr... it was strchr (I guess I am tierd)

--- Projects/libft/libft.h
+++ Projects/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:11:10 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/16 15:59:50 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/16 16:28:40 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-char	*ft_strstr(const char *haystack, const char *needle);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
