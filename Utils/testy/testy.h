f16c6f1298cba68d2946c6b0b902a954794dec46
Rémi Raumain <remiraumain@gmail.com>
2024-10-16 12:23:11 +0200

    Changed Testy lib for 42 norm

--- Utils/testy/testy.h
+++ Utils/testy/testy.h
@@ -6,15 +6,19 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 12:28:23 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/10 16:18:38 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/16 12:21:05 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TESTY_H
 # define TESTY_H
 
-# include "assertions.h"
-# include "limits.h"
-# include "test_macros.h"
+// MACROS
+void	run_test(void (*test)(void), const char *test_name);
+
+// ASSERTIONS
+void	assert_eq(int expected, int actual);
+void	assert_str_eq(char *expected, char *actual);
+void	assert_fail(char *message);
 
 #endif
