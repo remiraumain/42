959768b82b50a70c1742ffd439f66cb989297de1
Rémi Raumain <remiraumain@gmail.com>
2024-10-21 15:43:21 +0200

    stop using Testy

--- Projects/libft/ft_calloc.c
+++ Projects/libft/ft_calloc.c
@@ -6,20 +6,18 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 10:12:13 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/17 10:44:34 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/17 11:57:02 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-void *ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-	char	*ptr;
-	size_t	i;
+	unsigned char	*ptr;
+	size_t			i;
 
-	if (!size)
-		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
@@ -29,5 +27,5 @@ void *ft_calloc(size_t nmemb, size_t size)
 		ptr[i] = '\0';
 		i++;
 	}
-	return (void *)ptr;
+	return ((void *)ptr);
 }
