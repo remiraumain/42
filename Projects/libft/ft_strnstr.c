40364dd2e9698242a692017380420ae582a61612
Rémi Raumain <remiraumain@gmail.com>
2024-10-22 06:25:00 +0200

    Need to be fixed with gdb at 42

--- Projects/libft/ft_strnstr.c
+++ Projects/libft/ft_strnstr.c
@@ -6,28 +6,40 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 08:06:22 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/17 10:44:20 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/22 06:23:17 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+// H E L L O \0 (6)
+// L L O \0 (2)
+//
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
-	while (big[i])
+	if (!needle || len < 1)
+		return ((char *)haystack);
+	while (haystack[i])
 	{
 		j = 0;
-		while(big[j + i] == little[j])
+		while (haystack[j + i] == needle[j])
 		{
-			if (little[j] == '\0' || j == len)
-				return ((char *)&big[i]);
+			if (needle[j] == '\0' || j == len - 1)
+				return ((char *)&haystack[i]);
 			j++;
 		}
-		i = j + 1;
+		i++;
 	}
 	return (NULL);
+}
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%s", ft_strnstr("lorem ipsum dolor", "ispum", 3));
+	return (0);
 }
