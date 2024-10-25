b3ca8997cdbae1b86d4effe50652bfe38bf83653
Remi Raumain <remiraumain@gmail.com>
2024-10-25 15:44:41 +0200

    Bonus: ft_lstadd_front done

--- Projects/libft/libft.h
+++ Projects/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:11:10 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/25 14:33:49 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/25 14:48:54 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,3 +54,4 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
