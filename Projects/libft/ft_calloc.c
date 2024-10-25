11ca447db74f9df0b6ba58eadb6cae3a761ebea1
Remi Raumain <remiraumain@gmail.com>
2024-10-25 11:20:15 +0200

    Update: ft_calloc refacto

--- Projects/libft/ft_calloc.c
+++ Projects/libft/ft_calloc.c
@@ -6,26 +6,19 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 10:12:13 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/17 11:57:02 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/25 11:19:05 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned char	*ptr;
-	size_t			i;
 
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
-	i = 0;
-	while (i < nmemb * size)
-	{
-		ptr[i] = '\0';
-		i++;
-	}
+	ft_bzero(ptr, nmemb * size);
 	return ((void *)ptr);
 }
