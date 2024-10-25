7d76e581f5080a624433506e6dbd05446fef7247
Remi Raumain <remiraumain@gmail.com>
2024-10-25 17:01:07 +0200

    Bonus: ft_lstlast done

--- Projects/libft/libft.h
+++ Projects/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:11:10 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/25 15:46:25 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/25 16:12:53 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,3 +56,4 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
