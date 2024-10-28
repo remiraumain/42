32b3ff2132529c6fed6f3bdf7bd1231af71ad34d
Remi Raumain <remiraumain@gmail.com>
2024-10-28 14:17:53 +0100

    Bonus: ft_lstclear done

--- Projects/libft/libft.h
+++ Projects/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:11:10 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/28 09:36:21 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/28 09:42:24 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,3 +59,4 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
