9b559d82009536380cde195c5d9aaf20595dce4b
Remi Raumain <remiraumain@gmail.com>
2024-10-28 09:41:22 +0100

    Bonus: ft_lstdelone done

--- Projects/libft/libft.h
+++ Projects/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 09:11:10 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/28 08:33:59 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/28 09:36:21 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,3 +58,4 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
