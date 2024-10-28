2f8cc1af6928ccb9b37234619c60cca3630983c5
Rémi Raumain <remiraumain@gmail.com>
2024-10-28 16:48:53 +0100

    Refacto: ft_lstadd_front done

--- Projects/libft/ft_lstadd_front_bonus.c
+++ Projects/libft/ft_lstadd_front_bonus.c
@@ -6,14 +6,20 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 14:48:07 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/25 15:43:56 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/28 16:48:09 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Add the node ’new’ at the beginning of the list.
+ *
+ * @param lst The address of a pointer to the first link of a list
+ * @param new The address of a pointer to the node to be added to the list.
+ */
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	new->next = lst[0];
-	lst[0] = new;
+	new->next = *lst;
+	*lst = new;
 }
