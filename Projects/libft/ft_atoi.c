ec404aa3297691e6c26685d66fde26279b59309a
Rémi Raumain <remiraumain@gmail.com>
2024-10-29 15:28:50 +0100

    Comments and unused lib

--- Projects/libft/ft_atoi.c
+++ Projects/libft/ft_atoi.c
@@ -6,17 +6,15 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 11:27:49 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/22 11:54:07 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 15:28:23 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
 
 /**
  * @brief Convert a string to an integer.
  *
  * The ft_atoi() function converts the initial portion of the string pointed to
- *  by nptr to an integer. ft_atoi() does not detect errors.
+ * by nptr to an integer. ft_atoi() does not detect errors.
  *
  * @param nptr Pointer of the string to convert into an int.
  * @return The converted value or 0 on error.
