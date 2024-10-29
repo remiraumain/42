9eca93ae648e8ff534961b46ca76dd2330788184
Remi Raumain <remiraumain@gmail.com>
2024-10-29 09:54:07 +0100

    Ft_putendl_fd protected and comments added

--- Projects/libft/ft_putendl_fd.c
+++ Projects/libft/ft_putendl_fd.c
@@ -6,16 +6,25 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 11:34:02 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/25 11:37:41 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 09:53:10 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Output the string ’s’ to the given file descriptor followed by a 
+ * newline.
+ * 
+ * @param s The string to output.
+ * @param fd The file descriptor on which to write.
+ */
 void	ft_putendl_fd(char *s, int fd)
 {
 	unsigned int	i;
 
+	if (!s || !fd)
+		return ;
 	i = ft_strlen(s);
 	write(fd, s, i);
 	write(fd, "\n", 1);
