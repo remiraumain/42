28e50321088e6bb67231b1e83893547c842c9d09
Rémi Raumain <remiraumain@gmail.com>
2024-10-29 15:55:12 +0100

    Refacto to make it more readable and comments

--- Projects/libft/ft_memcmp.c
+++ Projects/libft/ft_memcmp.c
@@ -6,19 +6,32 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 06:07:48 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/17 08:04:44 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 15:53:50 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Compares two memory blocks.
+ *
+ * Compares the first `n` bytes of memory areas `s1` and `s2`. Returns an
+ * integer less than, equal to, or greater than zero if `s1` is found to be
+ * less than, equal to, or greater than `s2`.
+ *
+ * @param s1 Pointer to the first memory area.
+ * @param s2 Pointer to the second memory area.
+ * @param n Number of bytes to compare.
+ * @return An integer indicating the difference between the first differing
+ * bytes in `s1` and `s2`, or zero if they are identical up to `n` bytes.
+ */
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t			i;
 	unsigned char	*ptr1;
 	unsigned char	*ptr2;
 
-	if (!n)
+	if (n == 0)
 		return (0);
 	i = 0;
 	ptr1 = (unsigned char *)s1;
@@ -29,5 +42,5 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 			return (ptr1[i] - ptr2[i]);
 		i++;
 	}
-	return (ptr1[i - 1] - ptr2[i - 1]);
+	return (0);
 }
