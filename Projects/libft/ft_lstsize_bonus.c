dcdd27a897e4927d09fe60428c9f0a4718977084
Rémi Raumain <remiraumain@gmail.com>
2024-10-29 07:14:19 +0100

    Refacto: ft_lstsize done

--- Projects/libft/ft_lstsize_bonus.c
+++ Projects/libft/ft_lstsize_bonus.c
@@ -6,12 +6,19 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 15:45:39 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/25 16:07:56 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 07:13:49 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Counts the number of nodes in a list.
+ *
+ * @param lst The beginning of the list.
+ *
+ * @return The length of the list.
+ */
 int	ft_lstsize(t_list *lst)
 {
 	int	i;
