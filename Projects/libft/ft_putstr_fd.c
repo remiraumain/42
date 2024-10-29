547a00923702d571bc7e6bc7b20e66c7a69daa0e
Remi Raumain <remiraumain@gmail.com>
2024-10-29 09:58:09 +0100

    Ft_putstr_fd protected and comments added

--- Projects/libft/ft_putstr_fd.c
+++ Projects/libft/ft_putstr_fd.c
@@ -6,16 +6,24 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 11:29:50 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/25 11:33:28 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 09:57:31 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Output the string ’s’ to the given file descriptor.
+ * 
+ * @param s The string to output.
+ * @param fd The file descriptor on which to write.
+ */
 void	ft_putstr_fd(char *s, int fd)
 {
 	unsigned int	i;
 
+	if (!s || !fd)
+		return ;
 	i = ft_strlen(s);
 	write(fd, s, i);
 }
