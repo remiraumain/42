8b9437e7972e7dc525fc4128ddd4898d5c84981b
Rémi Raumain <remiraumain@gmail.com>
2024-10-29 20:20:57 +0100

    Refacto and comments ft_toupper, same as for ft_tolower

--- Projects/libft/ft_toupper.c
+++ Projects/libft/ft_toupper.c
@@ -6,15 +6,24 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 09:11:47 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/15 10:31:17 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 20:19:15 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Converts a lowercase letter to uppercase.
+ *
+ * If `c` is a lowercase letter, converts it to the corresponding uppercase
+ * letter. If `c` is not a lowercase letter, it is returned unchanged.
+ *
+ * @param c The character to convert.
+ * @return The uppercase equivalent of `c` if it is lowercase, otherwise `c`
+ * unchanged.
+ */
 int	ft_toupper(int c)
 {
-	if ((unsigned char)c >= 'a' && (unsigned char)c <= 'z')
-		return ((unsigned char)c - 32);
-	if (c == -1)
-		return ((char)c);
-	return ((unsigned char)c);
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	else
+		return (c);
 }
