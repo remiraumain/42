d3b5876c92b792cc16cbbb0b915da1500e07ce48
Rémi Raumain <remiraumain@gmail.com>
2024-10-29 15:22:58 +0100

    Comments

--- Projects/libft/ft_calloc.c
+++ Projects/libft/ft_calloc.c
@@ -6,15 +6,25 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 10:12:13 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/25 11:19:05 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 15:22:29 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Allocate and zero-initialize an array.
+ *
+ * Allocates memory for an array of `nmemb` elements of `size` bytes each
+ * and initializes all bytes to zero. Returns NULL if allocation fails.
+ *
+ * @param nmemb Number of elements to allocate.
+ * @param size Size of each element in bytes.
+ * @return Pointer to the allocated memory, or NULL on failure.
+ */
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	unsigned char	*ptr;
+	void	*ptr;
 
 	ptr = malloc(nmemb * size);
 	if (!ptr)
