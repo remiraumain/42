1a3960f300d7528f44701dd79887fb26c0948b53
Rémi Raumain <remiraumain@gmail.com>
2024-10-29 14:23:03 +0100

    Refacto and protected, issue with double free fixed

--- Projects/libft/ft_split.c
+++ Projects/libft/ft_split.c
@@ -6,115 +6,109 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 06:00:48 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/24 08:32:16 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 14:16:57 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-/**
- * @brief Return an array of the split strings.
- * 
- * Allocates (with malloc(3)) and returns an array of strings obtained by 
- * splitting ’s’ using the character ’c’ as a delimiter. The array must end 
- * with a NULL pointer.
- * 
- * @param s String  string to be split.
- * @param start Delimiter characte.
- * 
- * @return The array of new strings resulting from the split.NULL if the 
- * allocation fails.
- */
-static size_t	count_part(char const *s, char c)
+static int	count_words(char const *s, char c)
 {
-	size_t	i;
-	size_t	count;
-	int		was_in_part;
+	int	i;
+	int	count;
+	int	in_word;
 
 	i = 0;
 	count = 0;
-	was_in_part = 0;
+	in_word = 0;
 	while (s[i])
 	{
-		if (!was_in_part && s[i] != c)
+		if (s[i] != c && in_word == 0)
 		{
+			in_word = 1;
 			count++;
-			was_in_part = 1;
 		}
-		if (was_in_part && s[i] == c)
-			was_in_part = 0;
+		else if (s[i] == c)
+			in_word = 0;
 		i++;
 	}
 	return (count);
 }
 
-static size_t	partlen(char const *s_j, char c)
+static char	*malloc_word(const char *s, char c, int *start)
 {
-	size_t	i;
+	int		tmp;
+	int		len;
+	char	*word;
 
-	i = 0;
-	while (s_j[i] && s_j[i] != c)
-		i++;
-	return (i);
-}
-
-static void	clean(char **arr)
-{
-	size_t	i;
-
-	i = 0;
-	while (arr[i])
+	tmp = *start;
+	len = 0;
+	while (s[*start] && s[*start] != c)
 	{
-		i--;
-		free(arr[i]);
+		(*start)++;
+		len++;
 	}
-	free(arr);
-}
-
-static size_t	fill_part(char **arr, const char *s, size_t len)
-{
-	size_t	i;
-	size_t	j;
-	size_t	k;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	while (k < len)
-	{
-		arr[i][k] = s[j];
-		k++;
-		j++;
-	}
-	arr[i][k] = '\0';
-	return (k);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**arr;
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	arr = malloc((count_part(s, c) + 1) * sizeof(char *));
-	if (!arr)
+	if (!len)
 		return (NULL);
-	while (i < count_part(s, c))
+	word = ft_substr(s, tmp, len);
+	return (word);
+}
+
+static int	fill_arr(char **arr, int word_count, char const *s, char c)
+{
+	int	i;
+	int	start;
+
+	i = 0;
+	start = 0;
+	while (i < word_count)
 	{
-		while (s[j] == c)
-			j++;
-		arr[i] = malloc(partlen(&s[j], c) * sizeof(char) + 1);
+		while (s[start] == c)
+			start++;
+		arr[i] = malloc_word(s, c, &start);
 		if (!arr[i])
 		{
-			clean(arr);
-			return (NULL);
+			while (i > 0)
+			{
+				i--;
+				free(arr[i]);
+			}
+			free(arr);
+			return (-1);
 		}
-		j += fill_part(&arr[i], &s[j], partlen(&s[j], c));
 		i++;
 	}
 	arr[i] = 0;
+	return (0);
+}
+
+/**
+ * @brief Return an array of the split strings.
+ *
+ * Allocates (with malloc(3)) and returns an array of strings obtained by
+ * splitting ’s’ using the character ’c’ as a delimiter. The array must end
+ * with a NULL pointer.
+ *
+ * @param s String  string to be split.
+ * @param start Delimiter characte.
+ *
+ * @return The array of new strings resulting from the split.NULL if the
+ * allocation fails.
+ */
+char	**ft_split(char const *s, char c)
+{
+	char	**arr;
+	int		word_count;
+	int		success;
+
+	if (!s)
+		return (NULL);
+	word_count = count_words(s, c);
+	arr = malloc((word_count + 1) * sizeof(char *));
+	if (!arr)
+		return (NULL);
+	success = fill_arr(arr, word_count, s, c);
+	if (success == -1)
+		return (NULL);
 	return (arr);
 }
