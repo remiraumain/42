5bc22166647c97f0deb482b0ba60fdaf8068915d
Rémi Raumain <remiraumain@gmail.com>
2024-10-29 07:19:50 +0100

    Refacto: ft_lstlast done

--- Projects/libft/ft_lstlast_bonus.c
+++ Projects/libft/ft_lstlast_bonus.c
@@ -6,15 +6,24 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 16:10:53 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/25 17:01:56 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 07:19:35 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Return the last node of the list.
+ *
+ * @param lst The beginning of the list.
+ *
+ * @return The last node of the list.
+ */
 t_list	*ft_lstlast(t_list *lst)
 {
-	while (lst && lst->next)
+	if (!lst)
+		return (NULL);
+	while (lst->next)
 		lst = lst->next;
 	return (lst);
 }
