3665b82fa25be873bb2b4e7059350519baa02e0f
Remi Raumain <remiraumain@gmail.com>
2024-10-29 10:05:40 +0100

    Ft_putchar_fd comments added

--- Projects/libft/ft_putchar_fd.c
+++ Projects/libft/ft_putchar_fd.c
@@ -6,12 +6,18 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 11:23:31 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/25 11:28:25 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 10:05:09 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Output the character ’c’ to the given file descriptor.
+ * 
+ * @param c The character to output.
+ * @param fd The file descriptor on which to write.
+ */
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
