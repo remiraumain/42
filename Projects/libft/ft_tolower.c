c7fd20642a87385439c61d00c19b07f670412d62
Rémi Raumain <remiraumain@gmail.com>
2024-10-29 18:44:11 +0100

    Fixed to correctly handle c without casting for no reason and same for EOF

--- Projects/libft/ft_tolower.c
+++ Projects/libft/ft_tolower.c
@@ -6,15 +6,24 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 09:17:03 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/15 10:23:46 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/29 18:43:11 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Converts an uppercase letter to lowercase.
+ *
+ * If `c` is an uppercase letter, converts it to the corresponding lowercase
+ * letter. If `c` is not an uppercase letter, it is returned unchanged.
+ *
+ * @param c The character to convert.
+ * @return The lowercase equivalent of `c` if it is uppercase, otherwise `c`
+ * unchanged.
+ */
 int	ft_tolower(int c)
 {
-	if ((unsigned char)c >= 'A' && (unsigned char)c <= 'Z')
-		return ((unsigned char)c + 32);
-	if (c == -1)
-		return ((char)c);
-	return ((unsigned char)c);
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	else
+		return (c);
 }
