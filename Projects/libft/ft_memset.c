211ce87084e3ca2b61011327a8ed346751e03861
Remi Raumain <remiraumain@gmail.com>
2024-10-30 10:24:18 +0100

    Refacto ft_memset

--- Projects/libft/ft_memset.c
+++ Projects/libft/ft_memset.c
@@ -6,18 +6,17 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:41:17 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/11 17:33:03 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/30 10:23:06 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /**
- * @brief Replace n bytes of the memory at the location pointed by s with the
- *  constant byte c.
+ * @brief Replace `n` bytes of `s` with the constant byte `c`.
  *
- * The ft_memset() function fills the first n bytes of the memory area pointed
- *  to by s with the constant byte c.
+ * Fills the first `n` bytes of the memory area pointed to by `s` with the
+ * constant byte `c`.
  *
  * @param s Pointer of the memory's location.
  * @param c The constant byte that is used for the replacement.
@@ -25,15 +24,14 @@
  */
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t	i;
 	char	*ptr;
 
-	i = 0;
 	ptr = (char *)s;
-	while (i < n)
+	while (n)
 	{
-		ptr[i] = c;
-		i++;
+		*ptr = c;
+		ptr++;
+		n--;
 	}
 	return (s);
 }
