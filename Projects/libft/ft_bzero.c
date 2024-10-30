f9900ba253a70a679da4140a766d984430f79cea
Remi Raumain <remiraumain@gmail.com>
2024-10-30 10:20:00 +0100

    Refacto bzero

--- Projects/libft/ft_bzero.c
+++ Projects/libft/ft_bzero.c
@@ -6,32 +6,30 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 10:49:53 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/09 11:53:30 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/30 10:19:22 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /**
- * @brief Erase n bytes of the memory at the location pointed by s.
+ * @brief Erase `n` bytes of the memory at the location pointed by `s`.
  * 
- * The  ft_bzero()  function  erases  the data in the n bytes of the memory
- *  starting at the location pointed to by s, by writing zeros (bytes containing
- *  '\0') to that area.
+ * Erases  the data in the n bytes of the memory starting at the location
+ * pointed to by `s`, by writing zeros (bytes containing '\0') to that area.
  * 
  * @param s Pointer of the memory's location. 
  * @param n Number of bytes to erase.
  */
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
 	char	*ptr;
 
-	i = 0;
 	ptr = (char *)s;
-	while (i < n)
+	while (n)
 	{
-		ptr[i] = '\0';
-		i++;
+		*ptr = '\0';
+		ptr++;
+		n--;
 	}
 }
