1b30bd53906c8f2a9ed8da35cc3bb3e6215844ba
Rémi Raumain <remiraumain@gmail.com>
2024-10-30 07:30:09 +0100

    Refacto, better comments ft_strlcat

--- Projects/libft/ft_strlcat.c
+++ Projects/libft/ft_strlcat.c
@@ -6,49 +6,37 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 10:43:02 by rraumain          #+#    #+#             */
-/*   Updated: 2024/10/21 18:17:03 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/10/30 07:29:33 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /**
- * @brief Concatenates src to dst, ensuring null-termination.
+ * @brief Append a string to another with size limit.
  *
- * Appends src to dst, copying up to dstsize - strlen(dst) - 1 characters,
- * ensuring the result is null-terminated if there's space. Takes the total
- * size of the buffer (dstsize) into account, and returns the length of the
- * string they tried to create (initial length of dst + length of src).
+ * Appends the null-terminated string `src` to the end of `dst`, ensuring
+ * that the total length does not exceed `size` (including the null terminator).
+ * If `size` is less than or equal to the length of `dst`, `dst` is left
+ * unchanged, and `size + srclen` is returned.
  *
- * @param dst Destination string (must be null-terminated).
- * @param src Source string (must be null-terminated).
- * @param dstsize Total size of the destination buffer (including space for
- * 	null terminator).
- * @return Total length of the string it tried to create (dst + src).
- * If the return value is >= dstsize, truncation occurred.
+ * @param dst The destination string to append to.
+ * @param src The source string to append.
+ * @param size The total size of the destination buffer.
+ * @return The initial length of `dst` plus the length of `src`.
  */
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	j;
-	size_t	k;
-	size_t	cpy_len;
+	size_t	dstlen;
+	size_t	srclen;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	while (i < size && dst[i] != '\0')
-		i++;
-	while (src[j] != '\0')
-		j++;
-	if (size <= i)
-		return (size + j);
-	cpy_len = size - i - 1;
-	while (k < cpy_len && src[k] != '\0')
-	{
-		dst[i + k] = src[k];
-		k++;
-	}
-	dst[i + k] = '\0';
-	return (i + j);
+	srclen = ft_strlen(src);
+	if (!size)
+		return (srclen);
+	dstlen = ft_strlen(dst);
+	if (size <= dstlen)
+		return (size + srclen);
+	if (size > dstlen + 1)
+		ft_strlcpy(dst + dstlen, src, size - dstlen);
+	return (dstlen + srclen);
 }
