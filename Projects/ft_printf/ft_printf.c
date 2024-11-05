6df29fccc2cb7a5fc9a3de4a4021bc4dbc960ead
Rémi Raumain <remiraumain@gmail.com>
2024-11-05 07:15:07 +0100

    Fixed issue for %c

--- Projects/ft_printf/ft_printf.c
+++ Projects/ft_printf/ft_printf.c
@@ -6,11 +6,12 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 21:23:23 by rraumain          #+#    #+#             */
-/*   Updated: 2024/11/04 22:04:20 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/11/05 07:13:44 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 int	ft_printf(const char *format, ...)
 {
@@ -24,7 +25,7 @@ int	ft_printf(const char *format, ...)
 		if (*format == '%' && *(format + 1))
 		{
 			format++;
-			len += ft_selector(*format, args);
+			len += ft_selector(*format, &args);
 		}
 		else
 			len += ft_putchar(*format);
@@ -34,11 +35,9 @@ int	ft_printf(const char *format, ...)
 	return (len);
 }
 
-#include <stdio.h>
-
-int	main(void)
-{
-	ft_printf("ft_printf: %c %c %c \n", '0', 0, '1');
-	printf("printf: %c %c %c ", '0', 0, '1');
-	return (0);
-}
+// int	main(void)
+// {
+// 	ft_printf("ft_printf: %c %c %c \n", '3', 0, '1');
+// 	printf("printf: %c %c %c ", '3', 0, '1');
+// 	return (0);
+// }
--- Projects/ft_printf/ft_selector.c
+++ Projects/ft_printf/ft_selector.c
@@ -6,30 +6,31 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:58:35 by rraumain          #+#    #+#             */
-/*   Updated: 2024/11/04 22:01:45 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/11/05 07:13:12 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
-int	ft_selector(char selector, va_list args)
+int	ft_selector(char selector, va_list *args)
 {
 	if (selector == 'c')
-		return (ft_putchar(va_arg(args, int)));
+		return (ft_putchar(va_arg(*args, int)));
 	else if (selector == 's')
-		return (ft_putstr(va_arg(args, char *)));
+		return (ft_putstr(va_arg(*args, char *)));
 	else if (selector == 'p')
-		return (ft_putptr(va_arg(args, void *)));
+		return (ft_putptr(va_arg(*args, void *)));
 	else if (selector == 'd')
-		return (ft_putnbr(va_arg(args, int)));
+		return (ft_putnbr(va_arg(*args, int)));
 	else if (selector == 'i')
-		return (ft_putnbr(va_arg(args, int)));
+		return (ft_putnbr(va_arg(*args, int)));
 	else if (selector == 'u')
-		return (ft_putnbr(va_arg(args, unsigned int)));
+		return (ft_putnbr(va_arg(*args, unsigned int)));
 	else if (selector == 'x')
-		return (ft_puthex(va_arg(args, int), 0));
+		return (ft_puthex(va_arg(*args, int), 0));
 	else if (selector == 'X')
-		return (ft_puthex(va_arg(args, int), 1));
+		return (ft_puthex(va_arg(*args, int), 1));
 	else if (selector == '%')
 		return (ft_putchar('%'));
 	return (-1);
