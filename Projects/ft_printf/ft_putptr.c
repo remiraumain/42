fef7db881a98530ae42f794730dbf01d685a00d2
Remi Raumain <remiraumain@gmail.com>
2024-11-05 08:07:06 +0100

    Fixed p if NULL

--- Projects/ft_printf/ft_putptr.c
+++ Projects/ft_printf/ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 19:44:22 by rraumain          #+#    #+#             */
-/*   Updated: 2024/11/05 07:21:03 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/11/05 08:05:44 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int	ft_putptr(void *ptr)
 {
 	int	count;
 
+	if (!ptr)
+		return (ft_putstr("(nil)"));
 	count = 0;
 	count += ft_putstr("0x");
-	count += ft_puthex((unsigned long)ptr, 0);
+	count += ft_puthex((size_t)ptr, 0);
 	return (count);
 }
