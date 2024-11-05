322369db6d074c2ae078ee72174925b1a8bb1eee
Rémi Raumain <remiraumain@gmail.com>
2024-11-05 07:18:33 +0100

    Fixed issue for %s if NULL

--- Projects/ft_printf/ft_putstr.c
+++ Projects/ft_printf/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:00:52 by rraumain          #+#    #+#             */
-/*   Updated: 2024/11/04 22:02:14 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/11/05 07:17:24 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_putstr(char *s)
 	int	i;
 
 	if (!s)
-		return (-1);
+		return (write(1, "(null)", 6));
 	i = 0;
 	while (s[i])
 		i++;
