dbee47c42df24e6607f6c62f1ae71bf5e7fc1d73
remiraumain <remiraumain@gmail.com>
2024-12-17 21:26:26 +0100

    All leaks fixed (especially with ctrl + c)

--- Projects/minitalk/server.c
+++ Projects/minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 21:51:36 by rraumain          #+#    #+#             */
-/*   Updated: 2024/12/17 21:10:04 by rraumain         ###   ########.fr       */
+/*   Updated: 2024/12/17 21:23:50 by rraumain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,8 @@ static char	*convert_uchar(unsigned char c)
 static void	clear_memory(int stop_process)
 {
 	if (g_sigbyte.string)
-	{
 		free(g_sigbyte.string);
-		g_sigbyte.string = NULL;
-	}
+	g_sigbyte.string = NULL;
 	g_sigbyte.byte = 0;
 	g_sigbyte.count = 0;
 	if (stop_process)
@@ -40,13 +38,17 @@ static void	clear_memory(int stop_process)
 static int	add_to_string(unsigned char c)
 {
 	char	*new;
+	char	*tmp;
 
 	new = convert_uchar(c);
 	if (!new)
 		return (0);
+	tmp = g_sigbyte.string;
 	g_sigbyte.string = ft_strjoin(g_sigbyte.string, new);
 	free(new);
 	new = NULL;
+	free(tmp);
+	tmp = NULL;
 	if (!g_sigbyte.string)
 		return (0);
 	return (1);
@@ -63,6 +65,8 @@ static void	signal_handler(int signum, siginfo_t *info, void *context)
 		g_sigbyte.byte = g_sigbyte.byte << 1;
 	else if (signum == SIGUSR2)
 		g_sigbyte.byte = g_sigbyte.byte << 1 | 1;
+	else if (signum == SIGINT)
+		clear_memory(1);
 	g_sigbyte.count++;
 	if (g_sigbyte.count == 8)
 	{
@@ -89,6 +93,7 @@ int	main(void)
 	sigemptyset(&signal_action.sa_mask);
 	sigaction(SIGUSR1, &signal_action, NULL);
 	sigaction(SIGUSR2, &signal_action, NULL);
+	sigaction(SIGINT, &signal_action, NULL);
 	while (1)
 		pause();
 	return (0);
